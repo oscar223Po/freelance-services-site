948551f122dfacebd2ff59090d538c7a1c41a2f0
Oscar <oscaritoparapito225@mail.ru>
2024-02-21 23:06:32 +0600

    21/02/2024 | 23:05

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,6 +14,13 @@
             document.documentElement.classList.add(className);
         }));
     }
+    function functions_getHash() {
+        if (location.hash) return location.hash.replace("#", "");
+    }
+    function setHash(hash) {
+        hash = hash ? `#${hash}` : window.location.href.split("#")[0];
+        history.pushState("", "", hash);
+    }
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -83,6 +90,99 @@
     let _slideToggle = (target, duration = 500) => {
         if (target.hidden) return _slideDown(target, duration); else return _slideUp(target, duration);
     };
+    function tabs() {
+        const tabs = document.querySelectorAll("[data-tabs]");
+        let tabsActiveHash = [];
+        if (tabs.length > 0) {
+            const hash = functions_getHash();
+            if (hash && hash.startsWith("tab-")) tabsActiveHash = hash.replace("tab-", "").split("-");
+            tabs.forEach(((tabsBlock, index) => {
+                tabsBlock.classList.add("_tab-init");
+                tabsBlock.setAttribute("data-tabs-index", index);
+                tabsBlock.addEventListener("click", setTabsAction);
+                initTabs(tabsBlock);
+            }));
+            let mdQueriesArray = dataMediaQueries(tabs, "tabs");
+            if (mdQueriesArray && mdQueriesArray.length) mdQueriesArray.forEach((mdQueriesItem => {
+                mdQueriesItem.matchMedia.addEventListener("change", (function() {
+                    setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+                }));
+                setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+            }));
+        }
+        function setTitlePosition(tabsMediaArray, matchMedia) {
+            tabsMediaArray.forEach((tabsMediaItem => {
+                tabsMediaItem = tabsMediaItem.item;
+                let tabsTitles = tabsMediaItem.querySelector("[data-tabs-titles]");
+                let tabsTitleItems = tabsMediaItem.querySelectorAll("[data-tabs-title]");
+                let tabsContent = tabsMediaItem.querySelector("[data-tabs-body]");
+                let tabsContentItems = tabsMediaItem.querySelectorAll("[data-tabs-item]");
+                tabsTitleItems = Array.from(tabsTitleItems).filter((item => item.closest("[data-tabs]") === tabsMediaItem));
+                tabsContentItems = Array.from(tabsContentItems).filter((item => item.closest("[data-tabs]") === tabsMediaItem));
+                tabsContentItems.forEach(((tabsContentItem, index) => {
+                    if (matchMedia.matches) {
+                        tabsContent.append(tabsTitleItems[index]);
+                        tabsContent.append(tabsContentItem);
+                        tabsMediaItem.classList.add("_tab-spoller");
+                    } else {
+                        tabsTitles.append(tabsTitleItems[index]);
+                        tabsMediaItem.classList.remove("_tab-spoller");
+                    }
+                }));
+            }));
+        }
+        function initTabs(tabsBlock) {
+            let tabsTitles = tabsBlock.querySelectorAll("[data-tabs-titles]>*");
+            let tabsContent = tabsBlock.querySelectorAll("[data-tabs-body]>*");
+            const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+            const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
+            if (tabsActiveHashBlock) {
+                const tabsActiveTitle = tabsBlock.querySelector("[data-tabs-titles]>._tab-active");
+                tabsActiveTitle ? tabsActiveTitle.classList.remove("_tab-active") : null;
+            }
+            if (tabsContent.length) tabsContent.forEach(((tabsContentItem, index) => {
+                tabsTitles[index].setAttribute("data-tabs-title", "");
+                tabsContentItem.setAttribute("data-tabs-item", "");
+                if (tabsActiveHashBlock && index == tabsActiveHash[1]) tabsTitles[index].classList.add("_tab-active");
+                tabsContentItem.hidden = !tabsTitles[index].classList.contains("_tab-active");
+            }));
+        }
+        function setTabsStatus(tabsBlock) {
+            let tabsTitles = tabsBlock.querySelectorAll("[data-tabs-title]");
+            let tabsContent = tabsBlock.querySelectorAll("[data-tabs-item]");
+            const tabsBlockIndex = tabsBlock.dataset.tabsIndex;
+            function isTabsAnamate(tabsBlock) {
+                if (tabsBlock.hasAttribute("data-tabs-animate")) return tabsBlock.dataset.tabsAnimate > 0 ? Number(tabsBlock.dataset.tabsAnimate) : 500;
+            }
+            const tabsBlockAnimate = isTabsAnamate(tabsBlock);
+            if (tabsContent.length > 0) {
+                const isHash = tabsBlock.hasAttribute("data-tabs-hash");
+                tabsContent = Array.from(tabsContent).filter((item => item.closest("[data-tabs]") === tabsBlock));
+                tabsTitles = Array.from(tabsTitles).filter((item => item.closest("[data-tabs]") === tabsBlock));
+                tabsContent.forEach(((tabsContentItem, index) => {
+                    if (tabsTitles[index].classList.contains("_tab-active")) {
+                        if (tabsBlockAnimate) _slideDown(tabsContentItem, tabsBlockAnimate); else tabsContentItem.hidden = false;
+                        if (isHash && !tabsContentItem.closest(".popup")) setHash(`tab-${tabsBlockIndex}-${index}`);
+                    } else if (tabsBlockAnimate) _slideUp(tabsContentItem, tabsBlockAnimate); else tabsContentItem.hidden = true;
+                }));
+            }
+        }
+        function setTabsAction(e) {
+            const el = e.target;
+            if (el.closest("[data-tabs-title]")) {
+                const tabTitle = el.closest("[data-tabs-title]");
+                const tabsBlock = tabTitle.closest("[data-tabs]");
+                if (!tabTitle.classList.contains("_tab-active") && !tabsBlock.querySelector("._slide")) {
+                    let tabActiveTitle = tabsBlock.querySelectorAll("[data-tabs-title]._tab-active");
+                    tabActiveTitle.length ? tabActiveTitle = Array.from(tabActiveTitle).filter((item => item.closest("[data-tabs]") === tabsBlock)) : null;
+                    tabActiveTitle.length ? tabActiveTitle[0].classList.remove("_tab-active") : null;
+                    tabTitle.classList.add("_tab-active");
+                    setTabsStatus(tabsBlock);
+                }
+                e.preventDefault();
+            }
+        }
+    }
     function functions_FLS(message) {
         setTimeout((() => {
             if (window.FLS) console.log(message);
@@ -95,6 +195,44 @@
         return array.filter((function(item, index, self) {
             return self.indexOf(item) === index;
         }));
+    }
+    function dataMediaQueries(array, dataSetValue) {
+        const media = Array.from(array).filter((function(item, index, self) {
+            if (item.dataset[dataSetValue]) return item.dataset[dataSetValue].split(",")[0];
+        }));
+        if (media.length) {
+            const breakpointsArray = [];
+            media.forEach((item => {
+                const params = item.dataset[dataSetValue];
+                const breakpoint = {};
+                const paramsArray = params.split(",");
+                breakpoint.value = paramsArray[0];
+                breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+                breakpoint.item = item;
+                breakpointsArray.push(breakpoint);
+            }));
+            let mdQueries = breakpointsArray.map((function(item) {
+                return "(" + item.type + "-width: " + item.value + "px)," + item.value + "," + item.type;
+            }));
+            mdQueries = uniqArray(mdQueries);
+            const mdQueriesArray = [];
+            if (mdQueries.length) {
+                mdQueries.forEach((breakpoint => {
+                    const paramsArray = breakpoint.split(",");
+                    const mediaBreakpoint = paramsArray[1];
+                    const mediaType = paramsArray[2];
+                    const matchMedia = window.matchMedia(paramsArray[0]);
+                    const itemsArray = breakpointsArray.filter((function(item) {
+                        if (item.value === mediaBreakpoint && item.type === mediaType) return true;
+                    }));
+                    mdQueriesArray.push({
+                        itemsArray,
+                        matchMedia
+                    });
+                }));
+                return mdQueriesArray;
+            }
+        }
     }
     let formValidate = {
         getErrors(form) {
@@ -665,8 +803,67 @@
             var scrollPosition = window.scrollY;
             if (scrollPosition > 1) headingScroll.classList.add("_heading-scroll"); else headingScroll.classList.remove("_heading-scroll");
         }));
+        const courseItems = document.querySelector(".courses__items");
+        let data;
+        let startItem = 0;
+        let endItem = 3;
+        if (courseItems) loadCourseItems();
+        async function loadCourseItems() {
+            const response = await fetch("files/courses.json", {
+                method: "GET"
+            });
+            if (response.ok) {
+                const responseResult = await response.json();
+                data = responseResult;
+                initCourse(data, startItem, endItem);
+            } else alert("Error!");
+        }
+        function initCourse(data, startItem, endItem) {
+            const dataPart = data.items.slice(startItem, endItem);
+            dataPart.forEach((item => {
+                buildCourseItem(item);
+            }));
+            viewMore();
+        }
+        function buildCourseItem(item) {
+            let courseItemTemplate = ``;
+            courseItemTemplate += `<article data-id="${item.id}" class="course">`;
+            item.image ? courseItemTemplate += `<div class="course__image"><img src="${item.image}" alt="Image"></div>` : null;
+            courseItemTemplate += `<div class="course__content">`;
+            courseItemTemplate += `<a target="_blank" href="${item.url}" class="course__title">${item.title}</a>`;
+            courseItemTemplate += `<div class="course__form form-course">`;
+            courseItemTemplate += `<div class="form-course__name">${item.name}</div>`;
+            courseItemTemplate += `<div class="form-course__rate _icon-rate">${item.rate}</div>`;
+            courseItemTemplate += `<div class="form-course__cost">${item.price}</div>`;
+            courseItemTemplate += `</div>`;
+            courseItemTemplate += `<div class="course__labels labels-course">`;
+            courseItemTemplate += `<div class="labels-course__item _icon-clock">${item.clock}</div>`;
+            courseItemTemplate += `<div class="labels-course__item _icon-stack">${item.stack}</div>`;
+            courseItemTemplate += `<div class="labels-course__item _icon-level">${item.level}</div>`;
+            courseItemTemplate += `</div>`;
+            courseItemTemplate += `</div>`;
+            courseItemTemplate += `</article>`;
+            courseItems.insertAdjacentHTML("beforeend", courseItemTemplate);
+        }
+        document.addEventListener("click", documentActions);
+        function viewMore() {
+            const dataItemsLength = data.items.length;
+            const currentItems = document.querySelectorAll(".course").length;
+            const vewMore = document.querySelector(".courses__view-more");
+            currentItems < dataItemsLength ? vewMore.style.display = "inline-flex" : vewMore.style.display = "none";
+        }
+        function documentActions(e) {
+            const targetElement = e.target;
+            if (targetElement.closest(".courses__view-more")) {
+                startItem = document.querySelectorAll(".course").length;
+                endItem = startItem + 3;
+                initCourse(data, startItem, endItem);
+                e.preventDefault();
+            }
+        }
     }));
     window["FLS"] = false;
     isWebp();
+    tabs();
     digitsCounter();
 })();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -16,5 +16,80 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	});
 	// ================[ JavaScript Section ]================
+	const courseItems = document.querySelector(".courses__items");
+	let data;
+	let startItem = 0;
+	let endItem = 3;
+	if (courseItems) {
+		loadCourseItems();
+	}
+	async function loadCourseItems() {
+		const response = await fetch("files/courses.json", {
+			method: "GET"
+		});
+		if (response.ok) {
+			const responseResult = await response.json();
+			data = responseResult;
+			initCourse(data, startItem, endItem);
+		} else {
+			alert("Error!");
+		}
+	}
+	function initCourse(data, startItem, endItem) {
+		const dataPart = data.items.slice(startItem, endItem);
+		dataPart.forEach(item => {
+			buildCourseItem(item);
+		});
+		viewMore();
+	}
+	function buildCourseItem(item) {
+		let courseItemTemplate = ``;
+
+		courseItemTemplate += `<article data-id="${item.id}" class="course">`;
+
+		item.image ? courseItemTemplate += `<div class="course__image"><img src="${item.image}" alt="Image"></div>` : null;
+
+		courseItemTemplate += `<div class="course__content">`;
+
+		courseItemTemplate += `<a target="_blank" href="${item.url}" class="course__title">${item.title}</a>`;
+
+		courseItemTemplate += `<div class="course__form form-course">`;
+
+		courseItemTemplate += `<div class="form-course__name">${item.name}</div>`;
+		courseItemTemplate += `<div class="form-course__rate _icon-rate">${item.rate}</div>`;
+		courseItemTemplate += `<div class="form-course__cost">${item.price}</div>`;
+
+		courseItemTemplate += `</div>`;
+
+		courseItemTemplate += `<div class="course__labels labels-course">`;
+
+		courseItemTemplate += `<div class="labels-course__item _icon-clock">${item.clock}</div>`
+		courseItemTemplate += `<div class="labels-course__item _icon-stack">${item.stack}</div>`
+		courseItemTemplate += `<div class="labels-course__item _icon-level">${item.level}</div>`
+
+		courseItemTemplate += `</div>`;
+
+		courseItemTemplate += `</div>`;
+
+		courseItemTemplate += `</article>`;
+
+		courseItems.insertAdjacentHTML("beforeend", courseItemTemplate);
+	}
+	document.addEventListener("click", documentActions)
+	function viewMore() {
+		const dataItemsLength = data.items.length;
+		const currentItems = document.querySelectorAll(".course").length;
+		const vewMore = document.querySelector(".courses__view-more");
+		currentItems < dataItemsLength ? vewMore.style.display = 'inline-flex' : vewMore.style.display = 'none';
+	}
+	function documentActions(e) {
+		const targetElement = e.target;
+		if (targetElement.closest(".courses__view-more")) {
+			startItem = document.querySelectorAll(".course").length;
+			endItem = startItem + 3;
+			initCourse(data, startItem, endItem);
+			e.preventDefault();
+		}
+	}
 });
 //--------------------------------------------------------------
